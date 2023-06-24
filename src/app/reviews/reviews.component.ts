5cdfea2dea375ce6a664acf42d12c94fd80acc87
vnpugh <vnp19@yahoo.com>
2023-06-24 18:58:47 -0500

    Added navbar & footer to reviews page

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reviews',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./reviews.component.css']
 })
 export class ReviewsComponent {
+  destinations: any[] = [
+    { country: 'Aruba' },
+    { country: 'Barbados' },
+    { country: 'Curacao' },
+    { country: 'Jamaica' },
+    { country: 'St. Lucia' }
+    // Add more destination objects as needed
+  ];
+  searchQuery: string = '';
 
+
+  constructor(private router: Router) {}
+
+  searchDestinations() {
+    if (this.searchQuery && this.searchQuery.trim() !== '') {
+      this.router.navigate(['/destination'], { queryParams: { country: this.searchQuery } });
+    }
+  }
 }
